1813114961b14461f73432069a5e4dde3af17fb6
sverben <sverre@benedictus.nu>
2022-02-20 14:36:05 +0100

    initial commit

---
+++ src/main/java/nl/sverben/mclogin/MCLogin.java
@@ -0,0 +1,104 @@
+package nl.sverben.mclogin;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public final class MCLogin extends JavaPlugin implements Listener {
+    World empty;
+    Gson g = new Gson();
+
+    @Override
+    public void onEnable() {
+        empty = getServer().getWorld("empty");
+
+        if (empty == null) {
+            WorldCreator wc = new WorldCreator("empty");
+
+            wc.type(WorldType.FLAT);
+            wc.generator(new VoidGenerator());
+
+            empty = wc.createWorld();
+        }
+
+        getCommand("verify").setExecutor(new verifycmd(this));
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void playerJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+
+        p.setMetadata("LoggingIn", new FixedMetadataValue(this, true));
+        p.teleport(new Location(empty, 0, 0, 0));
+        p.setGameMode(GameMode.SPECTATOR);
+
+        String result = "{}";
+        try {
+            result = get("http://192.168.32.3:3000/api/genState?username=" + p.getName());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        getServer().getLogger().info(result);
+        JsonObject json = g.fromJson(result, JsonObject.class);
+
+        String url = json.get("auth").getAsString();
+        String state = json.get("state").getAsString();
+
+        p.sendMessage("Log in to DJO Minecraft on " + ChatColor.BOLD + url);
+        p.setMetadata("state", new FixedMetadataValue(this, state));
+    }
+
+    public static String get(String url) throws IOException {
+        HttpURLConnection httpClient =
+                (HttpURLConnection) new URL(url).openConnection();
+
+        httpClient.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(httpClient.getInputStream()))) {
+
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+
+            return response.toString();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return "{}";
+    }
+
+    @EventHandler
+    public void cmdProcess(PlayerCommandPreprocessEvent e) {
+        Player p = e.getPlayer();
+        if (e.getMessage().equals("/verify")) return;
+        if (!p.hasMetadata("LoggingIn")) return;
+        if (!p.getMetadata("LoggingIn").get(0).asBoolean()) return;
+
+        e.setCancelled(true);
+    }
+}
---
+++ src/main/java/nl/sverben/mclogin/verifycmd.java
@@ -0,0 +1,50 @@
+package nl.sverben.mclogin;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+
+import java.io.IOException;
+
+public class verifycmd implements CommandExecutor {
+    MCLogin plug;
+    Gson g = new Gson();
+
+    public verifycmd(MCLogin plug) {
+        this.plug = plug;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = (Player) sender;
+        if (!p.hasMetadata("LoggingIn")) return true;
+        if (!p.getMetadata("LoggingIn").get(0).asBoolean()) return true;
+        String state = p.getMetadata("state").get(0).asString();
+
+        JsonObject json = null;
+        try {
+            json = g.fromJson(plug.get("http://192.168.32.3:3000/api/checkState?state=" + state), JsonObject.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (json.get("ownerShip").isJsonNull()) {
+            p.sendMessage(ChatColor.RED + "Log in first!");
+            return true;
+        }
+        if (json.get("ownerShip").getAsBoolean()) {
+            p.teleport(plug.getServer().getWorld("lobby").getSpawnLocation());
+            plug.getServer().broadcastMessage(ChatColor.YELLOW + p.getName() + " logged into the server");
+            p.setMetadata("LoggingIn", new FixedMetadataValue(plug, false));
+            return true;
+        } else {
+            p.kickPlayer("Invalid login state!");
+            return true;
+        }
+    }
+}
